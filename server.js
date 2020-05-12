079c26bc70fd7b5c57e9bebc4373179644e08a76
jimufree <masahiro.kakami@jimu-free.com>
2020-05-12 19:55:07 +0900

    add feature mysql-getData

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 // ライブラリ読み込み
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const M_001 = require('./Api/M_001');
 
 //body-parserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 4000; // port番号を指定
+const port = process.env.PORT || 4000; // port番号を指定
 
 
 // GET http://localhost:4000/api/v1/
-app.get('/api/v1/',function(req,res){
+app.get('/api/v1/',(req, res) => {
     res.json({
         Massage:"Hello,world"
     });
+});
+app.get('/api/m_001',(req, res) => {
+  M_001.getData(req,res);
 });
 
 //サーバ起動
