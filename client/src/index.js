6e579a08f293d5ba9b82bbc0a8efbd20af459d18
jimufree <masahiro.kakami@jimu-free.com>
2020-05-14 11:54:02 +0900

    testA_user1

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Menu from './menu';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Menu />
   </React.StrictMode>,
   document.getElementById('root')
 );
