8aaf10b345428b2076c2266da61e00d15aadbe23
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-05-14 17:57:29 +0900

    mabuchi

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Sidebar from './components/Sidebar';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from "history"
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={hist}>
+    {/* <App /> */}
+    <Sidebar />
+  </Router>,
   document.getElementById('root')
 );
 
