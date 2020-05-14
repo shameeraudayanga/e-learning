d1f52996ef78448bb6d4d3ff6bb7111e3a16c1ed
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-05-14 18:24:13 +0900

    画像表示したい

---
+++ client/src/variables/data.jsx
@@ -0,0 +1,64 @@
+export const getData =
+  [
+    {
+      "contents_id":1,
+      "contents_detail_id":1,
+      "contents_name":"",
+      "contents_statement":"個人情報とは何を指すか。下記の4つの中から選びなさい。",
+      "choice1":"氏名",
+      "choice2":"電話番号",
+      "choice3":"住所",
+      "choice4":"勤務先",
+      "answer":3
+    },
+    {
+      "contents_id":1,
+      "contents_detail_id":2,
+      "contents_name":"",
+      "contents_statement":"個人情報とは何を指すか。下記の4つの中から選びなさい②．",
+      "choice1":"名前",
+      "choice2":"FAX番号",
+      "choice3":"郵便番号",
+      "choice4":"職場",
+      "answer":3
+    },
+    {
+      "contents_id":1,
+      "contents_detail_id":3,
+      "contents_name":"",
+      "contents_statement":"./image/slide1.jpg",
+      "choice1":"名前",
+      "choice2":"FAX番号",
+      "choice3":"郵便番号",
+      "choice4":"職場",
+      "answer":3
+    },
+    {
+      "contents_detail_id":4,
+      "contents_statement":"./img/default.jpg"
+    }
+    // {
+    //   "CustomerID":1,
+    //   "CustomerCode":"3001",
+    //   "CustomerName":"株式会社ノア",
+    //   "Address":"札幌市中央区",
+    //   "CustomerClassID":2,
+    //   "PrefecturalID":1
+    // },
+    // {
+    //   "CustomerID":2,
+    //   "CustomerCode":"3002",
+    //   "CustomerName":"株式会社JTB北海道",
+    //   "Address":"札幌市中央区",
+    //   "CustomerClassID":2,
+    //   "PrefecturalID":1
+    // },
+    // {
+    //   "CustomerID":3,
+    //   "CustomerCode":"3003",
+    //   "CustomerName":"国分株式会社",
+    //   "Address":"仙台市泉区",
+    //   "CustomerClassID":2,
+    //   "PrefecturalID":4
+    // }
+  ];
