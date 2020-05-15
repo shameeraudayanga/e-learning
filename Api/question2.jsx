b709142dc957cfe926fe9d9e30578655ca6ec1db
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-05-15 18:11:18 +0900

    operationcheck

--- Api/question2.jsx
+++ Api/question2.jsx
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-Parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 exports.question2 = (req,res) => {
-    const sql = require('SQL/question2.txt');
-    con.query(sql,(err, result, fields) => {
-        if (err) throw err;
-        res.json(result);
+    const mysql = require('mysql');
+
+    const con = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: ''
     });
 
+    con.connect((err) => {
+        if (err) throw err;
+        console.log('Connected!');
+    
+
+        const sql = require(',/Api/question2.txt');
+        con.query(sql,(err, result, fields) => {
+        if (err) throw err;
+        res.json(result);
+        });
+    });
 }
