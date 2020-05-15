743f2b162a84d6963b60d12f4f559cfe6504a752
shunichiro <shunichiro.takemura@force-corp.co.jp>
2020-05-15 14:31:40 +0900

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import FrontC from './frontC';
+//import { transformFromAstSync } from '@babel/core';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FrontC />
+    {/* <App /> */}
+    {/* <Question /> */}
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
