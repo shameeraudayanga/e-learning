130cc2bf63186f673cb1a9b70ba194fe57313168
YOSHIDARIE <https://github.com/YOSHIDARIE/4196.git>
2020-05-19 08:41:27 +0900

    answer3

--- client/src/Answer_yoshida.jsx
+++ client/src/Answer_yoshida.jsx
@@ -1,55 +1,25 @@
 import React, { useState } from 'react';
 // import './Answer.css'
 import Button from '@material-ui/core/Button';
-import { getData } from './variables/data';
 
-const Answer = () => {  
+const Answer = (props) => {  
   
   const [correctness, setCorrectness] = useState("");
-  const answer = getData.filter((data) => {return data.answer});
-
-  const handleClick = (e,selected) => {
-  if (selected === answer[0].props) {
+  
+　const handleClick = (selected) => {
+  if (selected === props.answer[0]) {
     setCorrectness = ("正解です");
   } else {
     setCorrectness = ("不正解です");
   }};
 
-  const current_data = getData.filter((data) => {
-    return data.contents_detail_id});
-
-  const current_choice1 = current_data.map((data) => (
-  <li key={data.contents_detail_id}>{data.choice1}</li>));
-  const current_choice2 = current_data.map((data) => (
-  <li key={data.contents_detail_id}>{data.choice2}</li>));
-  const current_choice3 = current_data.map((data) => (
-  <li key={data.contents_detail_id}>{data.choice3}</li>));
-  const current_choice4 = current_data.map((data) => (
-  <li key={data.contents_detail_id}>{data.choice4}</li>));
-  
   return (
     <div>
       <p>{correctness}</p>
-      <ul>
-        <li>
-          <Button varient="contained" onClick={e => handleClick(e,1)}>{current_choice1}</Button>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Button varient="contained" onClick={e => handleClick(e,2)}>{current_choice2}</Button>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Button varient="contained" onClick={e => handleClick(e,3)}>{current_choice3}</Button>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Button varient="contained" onClick={e => handleClick(e,4)}>{current_choice4}</Button>
-        </li>
-      </ul>
+          <Button varient="contained" onClick={() => handleClick(1)}>{props.choice1}</Button>
+          <Button varient="contained" onClick={() => handleClick(2)}>{props.choice2}</Button>
+          <Button varient="contained" onClick={() => handleClick(3)}>{props.choice3}</Button>
+          <Button varient="contained" onClick={() => handleClick(4)}>{props.choice4}</Button>
     </div>
   );
 } 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import S_002 from './S_002'
 import * as serviceWorker from './serviceWorker';
 
