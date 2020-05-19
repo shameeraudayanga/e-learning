efebf045e0d8e7a4b6abb16cfaaa4322a9a04198
sumireshimura <shimurasumire@gmail.com>
2020-05-19 11:35:16 +0900

    test

--- Api/menu.jsx
+++ Api/menu.jsx
@@ -9,16 +9,16 @@ exports.menu_screen = (req,res) => {
   const mysql = require('mysql');
 
   const con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: ''
+    host: 'localhost',
+    user: 'root',
+    password: ''
   });
 
   con.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
 
-    const sql = "select * from test.m_contents";
+    const sql = "select * from m_contents order by contents_type, contents_name";
     con.query(sql, (err, result, fields) => {
       if (err) throw err;
       res.json(result);
