f120f607ea6739ce020aa6e3ea8d731a5b7f56f1
momokatsuru <momoka.tsuru@force-corp.co.jp>
2020-05-19 18:05:48 +0900

    05/19

--- client/src/M_001.jsx
+++ client/src/M_001.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getData } from './variables/UserData';
-import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 
 const current_data = getData.filter((UserData) => {
