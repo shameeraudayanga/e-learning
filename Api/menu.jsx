09cd7ded9caee25284a6af4e9432fcf6e8bba1fd
sumireshimura <shimurasumire@gmail.com>
2020-05-19 12:48:58 +0900

    commit

--- Api/menu.jsx
+++ Api/menu.jsx
@@ -18,7 +18,7 @@ exports.menu_screen = (req,res) => {
     if (err) throw err;
     console.log('Connected!');
 
-    const sql = "select * from m_contents order by contents_type, contents_name";
+    const sql = "select * from test.m_contents order by contents_type, contents_name";
     con.query(sql, (err, result, fields) => {
       if (err) throw err;
       res.json(result);
