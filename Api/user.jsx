8e07d4c031725db8b6d236b68875d04d709a1616
yusukemabuchi <which.is.that@gmail.com>
2020-05-20 11:40:39 +0900

    2020.05.20

---
+++ Api/user.jsx
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+exports.getData = (req, res) => { 
+    const mysql = require('mysql');
+    
+    const con = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: ''
+    });
+
+    con.connect((err) => {
+      if (err) throw err;
+      console.log('Connected!');
+    
+    const sql = "SELECT user_id AS ユーザーID , user_name AS ユーザー名 ";
+    con.query(sql, (err, result, fields) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+}
--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const M_001 = require('./Api/M_001');
-const S_001 = require('./Api/S_001');
-const S_002 = require('./Api/S_002');
-const S_003 = require('./Api/S_003');
+// const M_001 = require('./Api/M_001');
+// const S_001 = require('./Api/S_001');
+// const S_002 = require('./Api/S_002');
+// const S_003 = require('./Api/S_003');
+const test = require('user.jsx')
 
 //body-parserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,22 +16,24 @@ const port = process.env.PORT || 4000; // port番号を指定
 
 
 // GET http://localhost:4000/api/v1/
-app.get('/api/v1/',(req, res) => {
+app.get('/v1/',(req, res) => {
     res.json({
         Massage:"Hello,world"
     });
-});
-app.get('/api/m_001',(req, res) => {
-  M_001.getData(req,res);
-});
-app.get('/api/s_001',(req, res) => {
-  S_001.slidescreen(req,res);
-});
-app.get('/api/s_002',(req, res) => {
-  S_002.verification_screen(req,res);
-});
-app.get('/api/s_003',(req, res) => {
-  S_003.end_screen(req,res);
+});　
+// app.get('/api/m_001',(req, res) => {
+//   M_001.getData(req,res);
+// });
+// app.get('/api/s_001',(req, res) => {
+//   S_001.slidescreen(req,res);
+// });
+// app.get('/api/s_002',(req, res) => {
+//   S_002.verification_screen(req,res);
+// });
+// app.get('/api/s_003',(req, res) => {
+//   S_003.end_screen(req,res);
+app.get('user.jsx', (req,res) => {
+  test.s_004(req,res);
 });
 
 
