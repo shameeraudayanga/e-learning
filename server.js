de009f3c01cc68b19e2d9d2f17235bc9849e0074
jimufree <masahiro.kakami@jimu-free.com>
2020-05-20 12:55:17 +0900

    merge master

--- server.js
+++ server.js
@@ -2,18 +2,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-// const M_001 = require('./Api/M_001');
-// const S_001 = require('./Api/S_001');
-// const S_002 = require('./Api/S_002');
-// const S_003 = require('./Api/S_003');
-const test = require('user.jsx')
-=======
 const menu = require('./Api/menu');
 const slide = require('./Api/slide');
 const question = require('./Api/question');
 const userName = require('./Api/userName');
->>>>>>> origin
 
 //body-parserの設定
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,22 +19,6 @@ app.get('/v1/',(req, res) => {
     res.json({
         Massage:"Hello,world"
     });
-<<<<<<< HEAD
-});　
-// app.get('/api/m_001',(req, res) => {
-//   M_001.getData(req,res);
-// });
-// app.get('/api/s_001',(req, res) => {
-//   S_001.slidescreen(req,res);
-// });
-// app.get('/api/s_002',(req, res) => {
-//   S_002.verification_screen(req,res);
-// });
-// app.get('/api/s_003',(req, res) => {
-//   S_003.end_screen(req,res);
-app.get('user.jsx', (req,res) => {
-  test.s_004(req,res);
-=======
 });
 app.get('/api/menu',(req, res) => {
   menu.getData(req,res);
@@ -55,7 +31,6 @@ app.get('/api/question',(req, res) => {
 });
 app.get('/api/userName',(req, res) => {
   userName.getData(req,res);
->>>>>>> origin
 });
 
 //サーバ起動
