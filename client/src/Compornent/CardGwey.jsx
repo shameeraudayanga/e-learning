75ac6b645143a232367c4e6ca59c2d542ca74fd9
jimufree <masahiro.kakami@jimu-free.com>
2020-05-20 11:04:04 +0900

    test

--- client/src/Compornent/CardGwey.jsx
+++ client/src/Compornent/CardGwey.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles({
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
   },
 });
 
-const Card_Gwey = (props) => {
+const CardGwey = (props) => {
   const classes = useStyles();
 
   return (
@@ -28,4 +28,4 @@ const Card_Gwey = (props) => {
   );
 }
 
-export default Card_Gwey;
+export default CardGwey;
--- client/src/View/S_001.jsx
+++ client/src/View/S_001.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { getData } from '../variables/data';
 import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Card_Gwey from '../Compornent/Card_Gwey';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+import CardGwey from '../Compornent/CardGwey';
 
 const useStyles = makeStyles((theme) => ({
     root: {
