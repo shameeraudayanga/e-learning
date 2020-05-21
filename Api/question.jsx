2c5f43ec65ea9aaa486f7bdced7aafabee0bc3f4
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-05-21 10:46:15 +0900

    as

--- Api/question.jsx
+++ Api/question.jsx
@@ -20,7 +20,7 @@ exports.getData = (req,res) => {
         console.log('Connected!');
     
 
-        const sql = "select C1.contents_id, contents_detail_id, C2.contents_name, contents_statement, choice1, choice2, choice3, choice4, answer from test.m_contents_detail as C1 join test.m_contents as C2 on C1.contents_id = C2.contents_id where C1.contents_id = 2 order by contents_detail_id";
+        const sql = "select C1.contents_id as コンテンツ番号 , contents_detail_id as コンテンツ詳細番号, C2.contents_name as コンテンツ詳細名, contents_statement as 主文, choice1 as 選択肢１, choice2 as 選択肢２, choice3 as 選択肢３, choice4 as 選択肢４, answer as 解答 from test.m_contents_detail as C1 join test.m_contents as C2 on C1.contents_id = C2.contents_id where C1.contents_id = 1 order by contents_detail_id";
         con.query(sql,(err, result, fields) => {
         if (err) throw err;
         res.json(result);
