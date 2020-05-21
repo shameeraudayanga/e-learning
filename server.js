d03b1ab5551612bee919e4a2548486b6296a8835
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-05-21 11:35:18 +0900

    mabuchi

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const menu = require('./Api/menu');
-// const slide = require('./Api/slide');
-// const question = require('./Api/question');
+const menu = require('./Api/menu');
+const slide = require('./Api/slide');
+const question = require('./Api/question');
 const userName = require('./Api/userName');
 
 //body-parserの設定
@@ -20,15 +20,15 @@ app.get('/v1/',(req, res) => {
         Massage:"Hello,world"
     });
 });
-// app.get('/api/menu',(req, res) => {
-//   menu.getData(req,res);
-// });
-// app.get('/api/slide',(req, res) => {
-//   slide.getData(req,res);
-// });
-// app.get('/api/question',(req, res) => {
-//   question.getData(req,res);
-// });
+app.get('/api/menu',(req, res) => {
+  menu.getData(req,res);
+});
+app.get('/api/slide',(req, res) => {
+  slide.getData(req,res);
+});
+app.get('/api/question',(req, res) => {
+  question.getData(req,res);
+});
 app.get('/api/userName',(req, res) => {
   userName.getData(req,res);
 });
