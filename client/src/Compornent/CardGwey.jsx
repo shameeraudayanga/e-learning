063bb476ee2d955d0d541e9600c23214e6e6851e
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-05-22 14:07:52 +0900

    commit

--- client/src/Compornent/CardGwey.jsx
+++ client/src/Compornent/CardGwey.jsx
@@ -3,14 +3,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 // import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
+import { CardActionArea, CardContent } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 300,
   },
   media: {
-    width:300,
-    height: 300,
+    width:50,
+    height: 50,
     position: 'absolute',
     top:'20%',
     left:'30%',
