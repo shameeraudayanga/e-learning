28b73c59bd7f08141b3c8c776943503a38b3443d
momokatsuru <momoka.tsuru@force-corp.co.jp>
2020-05-22 11:03:35 +0900

    test

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Toolbar from '@material-ui/core/Toolbar';
-import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import CreateIcon from '@material-ui/icons/Create';
 import ImportContactsIcon from '@material-ui/icons/ImportContacts';
-import { ListItemText } from '@material-ui/core';
-import { getContentsData } from '../variables/M_Contents';
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    position: 'fixed',
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    top: 67,
   },
   hide: {
     display: 'none',
@@ -34,39 +25,17 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
-    top: 67,
   },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
+  listtop: {
+    top: 60,
+  }
 }));
-export default function PersistentDrawerLeft (props) {
+export default function PersistentDrawerLeft(props) {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const [open] = React.useState(true);
+
   return (
     <div className={classes.root}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -76,23 +45,27 @@ export default function PersistentDrawerLeft (props) {
           paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-        {getContentsData.map((data) => (
-          <Link to={data.contents_name} style={{boxShadow:'none',textDecoration:'none',color:'inherit',fontFamily:'Montserrat, sans-serif'}}>
-            <ListItem button key={data.contents_id}>
-              <ListItemIcon>{data.contents_type === 1 ? <ImportContactsIcon /> : <CreateIcon />}</ListItemIcon>
-              <ListItemText primary={data.contents_name} />
+        <List className = {classes.listtop}>
+
+          <ListItem button>
+          <ListItemIcon><ImportContactsIcon /></ListItemIcon>
+            <li>{props.curriculum}</li>
             </ListItem>
-          </Link>
-          ))}
+
+          <ListItem button>
+              <ListItemIcon><CreateIcon /></ListItemIcon>
+              <li>{props.test}</li>
+            </ListItem>
+
         </List>
       </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
     </div>
   );
 }
