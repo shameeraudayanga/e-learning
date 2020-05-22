e354dd785bc12303f52b73d0fd4084cd9ea93076
momokatsuru <momoka.tsuru@force-corp.co.jp>
2020-05-22 11:51:08 +0900

    tsuru

---
+++ client/src/Variables/M_Contents_Type.jsx
@@ -0,0 +1,11 @@
+export const getTypeData =
+  [
+    {
+        "contents_type_id":1,
+        "contents_type_name":"スライド",
+    },
+    {
+        "contents_type_id":2,
+        "contents_type_name":"問題",
+    }
+  ];
