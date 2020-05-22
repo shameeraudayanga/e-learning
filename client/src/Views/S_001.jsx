3d993c0345bb506604f17923459e9a885eb403d7
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-05-22 14:22:51 +0900

    統合

--- client/src/Views/S_001.jsx
+++ client/src/Views/S_001.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { getData } from '../Variables/frontA';
 import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
-// import Card from '@material-ui/core/Card';
+import Card from '@material-ui/core/Card';
 // import CardActions from '@material-ui/core/CardActions';
 // import CardActionArea from '@material-ui/core/CardActionArea';
 // import CardMedia from '@material-ui/core/CardMedia';
