705572edc410781d6c2bbc2e1f7cb91af72b899b
hayatefukuda <hayate.fukuda@force-corp.co.jp>
2020-05-22 18:25:46 +0900

    sidebar

--- client/src/Compornent/Sidebar.jsx
+++ client/src/Compornent/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link , Route } from 'react-router-dom'
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -16,6 +16,8 @@ import CreateIcon from '@material-ui/icons/Create';
 import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 import { ListItemText } from '@material-ui/core';
 import { getContentsData } from '../Variables/M_Contents';
+import S_001 from '../Views/S_001';
+import S_002 from '../Views/S_002';
 
 const drawerWidth = 240;
 
@@ -44,6 +46,22 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
@@ -100,6 +118,14 @@ export default function PersistentDrawerLeft (props) {
           ))}
         </List>
       </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}>
+        <div className={classes.drawerHeader} />
+        <Route path="/s_001" component={S_001} />
+        <Route path="/s_002" component={S_002} />
+      </main>
     </div>
   );
 }
