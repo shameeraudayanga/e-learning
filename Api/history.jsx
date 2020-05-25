38f2704cb9015ac1e9746fdee8041041d2cb2d9b
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-05-25 18:08:39 +0900

    insert

--- Api/history.jsx
+++ Api/history.jsx
@@ -1,10 +1,9 @@
-applicationCache.port('/',(req,res) => {
-    const sql = "INSERT INTO t_history SET ?";
+    app.post('/',(req,res) => {
+        const sql = "INSERT INTO e_learning.t_history(contents_id,attending_user,attending_date) VALUES(2,3,NOW())";
 
-    con.query(sql,req.body,function(err,result,fields){
-        if(err)throw err;
-        console.log(result);
-        res.send('登録が完了しました');
+        con.query(sql,req.body,function(err,result,fields){
+            if(err)throw err;
+            console.log(result);
+            res.send('登録が完了しました');
+        });
     });
-    
-});
