9a24516f8c857830013dd019f4169a0b343958ff
Hkazu1104 <kazumasa.hara@forse-corp.co.jp>
2020-05-25 17:58:33 +0900

    ボタン完成

--- client/src/Compornent/Answer.jsx
+++ client/src/Compornent/Answer.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { Grid } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
 import '../Assets/Answer.css'
 
 const Answer = (props) => {  
-  
+
   const [correctness, setCorrectness] = useState("");
   
 　const handleClick = (selected) => {
@@ -14,20 +15,34 @@ const Answer = (props) => {
     setCorrectness("不正解です");
   }};
 
+  const useStyles = makeStyles( () => ({
+    choice: {
+      backgroundColor: "#4791DB",
+      height: '50px',
+      width: '200px',
+      fontSize: '20px',
+      '&:hover': {
+        backgroundColor: "#4791DB",
+      }
+    }
+  }));
+  
+  const classes = useStyles();
+
   return (
     <div>
       <Grid className="grid" container spacing={5}>
-        <Grid className="button" item xs={6}> 
-          <Button variant="contained" color="primary" disableElevation className='choice' onClick={() => handleClick(1)}>{props.choice1}</Button>
+        <Grid className="button" item xs={6}>
+          <Button className={classes.choice} onClick={() => handleClick(1)}>{props.choice1}</Button>
         </Grid>
         <Grid item xs={6}>
-          <Button variant="contained" color="primary" disableElevation className='choice' onClick={() => handleClick(2)}>{props.choice2}</Button>
+          <Button className={classes.choice} onClick={() => handleClick(2)}>{props.choice2}</Button>
         </Grid>
         <Grid className="button" item xs={6}>
-          <Button variant="contained" color="primary" disableElevation className='choice' onClick={() => handleClick(3)}>{props.choice3}</Button>
+          <Button className={classes.choice} onClick={() => handleClick(3)}>{props.choice3}</Button>
         </Grid>
         <Grid item xs={6}>
-          <Button variant="contained" color="primary" disableElevation className='choice' onClick={() => handleClick(4)}>{props.choice4}</Button>
+          <Button className={classes.choice} onClick={() => handleClick(4)}>{props.choice4}</Button>
         </Grid>
       </Grid>
       <p className="correctness">{correctness}</p>
