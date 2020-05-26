91819e1a15b85847e480ad44a42d56dd7a13eb1b
okazakitakuto01 <okazakitakuto01@gmail.com>
2020-05-26 17:48:07 +0900

    test

--- Api/history.jsx
+++ Api/history.jsx
@@ -1,29 +1,29 @@
-// ライブラリ読み込み
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+// // ライブラリ読み込み
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-//body-parserの設定
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// //body-parserの設定
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-exports.getData = (req, res) => {
-  const mysql = require('mysql');
+// exports.getData = (req, res) => {
+//   const mysql = require('mysql');
 
-  const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
-  });
+//   const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: ''
+//   });
 
-  app.post('/', (req, res) => {
-	const sql = "INSERT INTO T_history SET history_id = 1 attending_user = 1 contents_id = 1 attending_date = 1;"
-	con.query(sql,req.body,function(err, result, fields){
-		if (err) throw err;
-		console.log(result);
-		res.send('登録が完了しました');
-	});
-});
+//   app.post('/', (req, res) => {
+// 	const sql = "INSERT INTO T_history SET history_id = 1 attending_user = 1 contents_id = 1 attending_date = 1;"
+// 	con.query(sql,req.body,function(err, result, fields){
+// 		if (err) throw err;
+// 		console.log(result);
+// 		res.send('登録が完了しました');
+// 	});
+// });
 
 
-}
+// }
