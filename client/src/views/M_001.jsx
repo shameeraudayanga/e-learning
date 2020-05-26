816bd8c42d262af54cf561323be9000453a46a47
jimufree <masahiro.kakami@jimu-free.com>
2020-05-26 10:23:38 +0900

    sample

--- client/src/views/M_001.jsx
+++ client/src/views/M_001.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import { getUserData } from '../Variables/M_User';
 import Header from '../Compornent/Header';
 import Sidebar from '../Compornent/Sidebar';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const current_data = getUserData.filter((M_User) => {
     return M_User.user_id === 1;
     });
 
 const M_001 = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => getUserData());
+
+  const getUserData = () => {
+    axios
+      .get('/api/userName/1')
+      .then(response => {
+        console.log([response.data]);
+      })
+      .catch(() => {
+        console.log('connected error');
+      })
+  } 
+
     return (
         <div className="M_001">
             <Sidebar />
