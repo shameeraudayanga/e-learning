44894e3a44d5a9de570c4f48009be1f465c7d87f
dwnkakami <masahiro.kakami@dwn.co.jp>
2020-05-26 12:01:06 +0900

    mabuchi

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link , Route } from 'react-router-dom'
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Toolbar from '@material-ui/core/Toolbar';
+import Toolbar from '@material-ui/core/Toolba';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import IconButton from '@material-ui/core/IconButton';
@@ -15,17 +15,18 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import CreateIcon from '@material-ui/icons/Create';
 import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 import { ListItemText } from '@material-ui/core';
-import { getContentsData } from '../variables/M_Contents';
-
+import { getContentsData } from '../Variables/M_Contents';
+import S_002 from '../Views/S_002';
+import S_001 from '../Views/S_001';
 const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    top: 67,
+    top: 80,
+    position: "fixed",
   },
   hide: {
     display: 'none',
@@ -45,22 +46,33 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
   },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 }));
-
 export default function PersistentDrawerLeft (props) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
-
   const handleDrawerOpen = () => {
     setOpen(true);
   };
-
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
-
   return (
     <div className={classes.root}>
         <Toolbar>
@@ -91,7 +103,7 @@ export default function PersistentDrawerLeft (props) {
         <Divider />
         <List>
         {getContentsData.map((data) => (
-          <Link to={data.contents_name} style={{boxShadow:'none',textDecoration:'none',color:'inherit',fontFamily:'Montserrat, sans-serif'}}>
+           <Link to={data.contents_type === 1 ? "/s_001" : "/s_002"} style={{boxShadow:'none',textDecoration:'none',color:'inherit',fontFamily:'Montserrat, sans-serif'}}>
             <ListItem button key={data.contents_id}>
               <ListItemIcon>{data.contents_type === 1 ? <ImportContactsIcon /> : <CreateIcon />}</ListItemIcon>
               <ListItemText primary={data.contents_name} />
@@ -100,6 +112,14 @@ export default function PersistentDrawerLeft (props) {
           ))}
         </List>
       </Drawer>
+      <main
+        className={clsx(classes.content, {
+        [classes.contentShift]: open,
+        })}>
+        <div className={classes.drawerHeader} />
+        <Route path="/s_002" component={S_002} />
+        <Route path="/s_001" component={S_001} />
+      </main>
     </div>
   );
 }
--- client/src/views/M_001.jsx
+++ client/src/views/M_001.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { getUserData } from '../variables/M_User';
-import { getContentsData } from '../variables/M_Contents';
-import { getTypeData } from '../variables/M_Contents_Type';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
+import { getUserData } from '../Variables/M_User';
+import Header from '../Compornent/Header';
+import Sidebar from '../Compornent/Sidebar';
+import axios from 'axios';
+import { response } from 'express';
+import { useEffect } from 'react';
 
-const current_data = getUserData.filter((M_User) => {
-    return M_User.user_id === 1;
-    });
+// const current_data = getUserData.filter((M_User) => {
+//     return M_User.user_id === 1;
+//     });
 
 const M_001 = () => {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => getUserData());
+
+    const getUserData = () => {
+        if(user.length === 0){
+        axios
+        .get('/api/userName/1')
+        .then(response => {
+            // Console.log([response.data]);
+            setUser(response.data)
+        })
+        .catch(() => {
+            Console.log('connected error');
+        })
+    }
+    }
     return (
         <div className="M_001">
             <Sidebar />
-            {current_data.map((M_User) => (
+            {user.map((M_User) => (
             <Header key={M_User.user_id} 
             contents={M_User.user_name} />  
             ))}
