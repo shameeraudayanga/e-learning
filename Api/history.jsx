c28273fffa09273f95b0e09e843fab1df54de235
gennsou <ichigo5932@gmail.com>
2020-05-26 11:29:08 +0900

    satou

--- Api/history.jsx
+++ Api/history.jsx
@@ -14,15 +14,22 @@ exports.getData = (req, res) => {
         password: ''
     });
 
-    con.connect((err) => {
-        if (err) throw err;
-        console.log('Connected!');
-
-        const sql ="SELECT history_id, attending_user, contents_id, NOW(attending_date) FROM t_history WHERE history_id = " + req;
-
-        con.query(sql, (err, result, fields) => {
-            if (err) throw err;
-            res.json(result);
+    // con.connect((err) => {
+        app.post('/', (req, res) => {
+            const sql = "INSERT INTO historyes SET ?"
+            con.query(sql,req.body,function(err, result, fields){
+                if (err) throw err;
+                console.log(result);
+            });
         });
-    });
+//         if (err) throw err;
+//         console.log('Connected!');
+
+//         const sql ="SELECT history_id, attending_user, contents_id, NOW(attending_date) FROM t_history WHERE history_id = " + req;
+
+//         con.query(sql, (err, result, fields) => {
+//             if (err) throw err;
+//             res.json(result);
+//         });
+//     });
 }
