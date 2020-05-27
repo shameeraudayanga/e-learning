bf780dd792945e9024251fb3444013662a8b7a25
hayatefukuda <hayate.fukuda@force-corp.co.jp>
2020-05-27 17:16:38 +0900

    frontB

---
+++ client/src/views/M_001.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../Compornent/Header';
+import Sidebar from '../Compornent/Sidebar';
+import axios from 'axios';
+import { useState , useEffect } from 'react';
+
+const M_001 = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => getUserData());
+
+  const getUserData = () => {
+    if(user.length === 0) {
+      axios
+        .get('/api/userName/1')
+        .then(response => {
+          setUser(response.data);
+          
+        })
+        .catch(() => {
+          console.log('connected error');
+        })
+    }
+  }
+
+    return (
+        <div className="M_001">
+            <Sidebar />
+            {user.map((data) => (
+            <Header key={data.user_id} 
+            contents={data.user_name} />  
+            ))}
+        </div>
+    );
+}
+export default M_001;
