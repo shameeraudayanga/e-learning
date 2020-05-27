67fa89593652f2df194b4f83919808e9d51cbbef
gennsou <ichigo5932@gmail.com>
2020-05-27 17:59:43 +0900

    satou

---
+++ Api_aws/userName.jsx
@@ -0,0 +1,40 @@
+// ライブラリ読み込み
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysqlssh = require('mysql-ssh');
+const fs = require('fs');
+const path = require('path');
+
+//body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+exports.getData = (req, res) => {
+
+ mysqlssh.connect(
+   {
+     host: '54.250.240.189',
+     user: 'ubuntu',
+     privatekey: fs.readFileSync(path.resolve(__dirname,'../') + '/key_elearning.pem')
+   },
+   {
+     host: 'localhost',
+     user: 'root',
+     password: '',
+     database: 'e_learning'
+   }
+ )
+ .then(client => {
+   const sql = "select * from e_learning.m_user where user_id = " + req;
+   client.querly(sql, (err,results,fields) => {
+     if(err) throw err
+     console.log(results);
+     res.json(results);
+     mysqlssh.close()
+   })
+ })
+ .catch(err => {
+   console.log(err);
+ })
+}
