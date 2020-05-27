106698b29ed12f03ae267ba4d1f83b92dcac35f2
hayatefukuda <hayate.fukuda@force-corp.co.jp>
2020-05-27 09:43:13 +0900

    frontB

---
+++ client/src/Views/M_001.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { getUserData } from '../variables/M_User';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+
+const current_data = getUserData.filter((M_User) => {
+    return M_User.user_id === 1;
+    });
+
+const M_001 = () => {
+    return (
+        <div className="M_001">
+            <Sidebar />
+            {current_data.map((M_User) => (
+            <Header key={M_User.user_id} 
+            contents={M_User.user_name} />  
+            ))}
+        </div>
+    );
+}
+export default M_001;
