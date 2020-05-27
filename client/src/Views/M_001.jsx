b3b5df69e559c896896f5863968956b5db1ac6cd
momokatsuru <momoka.tsuru@force-corp.co.jp>
2020-05-27 14:34:05 +0900

    test

--- client/src/Views/M_001.jsx
+++ client/src/Views/M_001.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { getUserData } from '../Variables/M_User';
+// import { getUserData } from '../Variables/M_User';
 import Header from '../Compornent/Header';
 import Sidebar from '../Compornent/Sidebar';
-const current_data = getUserData.filter((M_User) => {
-    return M_User.user_id === 1;
-    });
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+// const current_data = getUserData.filter((M_User) => {
+//     return M_User.user_id === 1;
+//     });
+
 const M_001 = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => getUserData());
+
+  const getUserData = () => {
+    if(user.length === 0) {
+      axios
+        .get('/api/userName/1')
+        .then(response => {
+          // console.log([response.data]);
+          setUser(response.data);
+          
+        })
+        .catch(() => {
+          console.log('connected error');
+        })
+    }
+  }
+
     return (
         <div className="M_001">
             <Sidebar />
-            {current_data.map((M_User) => (
-            <Header key={M_User.user_id} 
-            contents={M_User.user_name} />  
+            {user.map((data) => (
+            <Header key={data.user_id} 
+            contents={data.user_name} />  
             ))}
         </div>
     );
