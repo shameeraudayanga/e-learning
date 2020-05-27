1d2bcec54accd50074105e68a6d338ac647fe071
horikawa1218 <daichi.horikawa@force-corp.co.jp>
2020-05-27 18:05:17 +0900

    AWS

---
+++ Api_aws/userName.jsx
@@ -0,0 +1,41 @@
+// ライブラリ読み込み
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysqlssh = require('mysql-ssh');
+const fs = require('fs');
+const path = require('path');
+
+
+//body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+exports.getData = (req, res) => {
+  mysqlssh.connect(
+    {
+      host: '54.250.240.189',
+      user: 'ubuntu',
+      privateKey: fs.readFileSync(path.resolve(__dirname,'../') + '/Key_elearning.pem')
+    },
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'e_learning'
+    }
+  )
+  .then(client => {
+    const sql ="select * from e_learning.m_user where user_id = " + req;
+    client.query(sql,(err,results,fields) => {
+      if(err) throw err
+      // console.log(results);
+      res.json(results);
+      mysqlssh.close()
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+}
