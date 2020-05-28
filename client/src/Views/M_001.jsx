24cd8f7b045db7019e60166944e2baf2a265c6bb
shinohara <nanami.shinohara@force-corp.co.jp>
2020-05-28 09:58:12 +0900

    add

--- client/src/Views/M_001.jsx
+++ client/src/Views/M_001.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-<<<<<<< HEAD
+import React, { useState } from 'react';
 import { getUserData } from '../Variables/M_User';
-import { getContentsData } from '../Variables/M_Contents';
-=======
->>>>>>> 7f4c8a0645873dd1cdbd419e1f98605453b68a1a
 import Header from '../Compornent/Header';
 import Sidebar from '../Compornent/Sidebar';
 import axios from 'axios';
-import { useState , useEffect } from 'react';
+import { useEffect } from 'react';
+
+// const current_data = getUserData.filter((M_User) => {
+//     return M_User.user_id === 1;
+//     });
 
 const M_001 = () => {
-  const [user, setUser] = useState([]);
+    const [user, setUser] = useState([]);
 
-  useEffect(() => getUserData());
+    useEffect(() => getUserData());
 
-  const getUserData = () => {
-    if(user.length === 0) {
-      axios
-        .get('/api/userName/1')
-        .then(response => {
-          setUser(response.data);
-          
-        })
-        .catch(() => {
-          console.log('connected error');
-        })
-    }
-  }
+    const getUserData = () => {
+        if(user.length === 0){
+        
+        axios
+            .get('/api/userName/1')
+            .then(response => {
+                // console.log([response.data]);
+                setUser([response.data]);
+            })
+            .catch(() => {
+                console.log('connected error');
+            })
+    }}
 
     return (
         <div className="M_001">
             <Sidebar />
-            {user.map((data) => (
-            <Header key={data.user_id} 
-            contents={data.user_name} />  
+            {user.map((M_User) => (
+            <Header key={M_User.user_id} 
+            contents={M_User.user_name} />  
             ))}
         </div>
     );
